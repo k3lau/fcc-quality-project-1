80e7829906c3926cddfa663108561d627f318578
k3lau <13735949+k3lau@users.noreply.github.com>
2022-01-16 12:55:50 -0500

    Handle invalid number and unit

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -4,11 +4,14 @@ function ConvertHandler() {
     let regex = /[+-]?(\d+)?(\.\d+)?(\/[+-]?(\d+)?(\.\d+)?)?/g;
     result = input.match(regex);
     console.log(`getNum input ${input} and ${result}`)
+    //Check for invalid number like 3/3/3 or 2.2.2
+    //  The match will be '3/3' and '/3' or '2.2' and '.2'
     if (result.length > 1 && result[1] !== '') {
       return "invalid number"
     }
     result = input.match(regex)[0];
 
+    //Parse fraction string to float number
     if (result.indexOf("/") > -1) {
       let fractionNum = result.split('/');
       result = parseFloat(fractionNum[0]) / parseFloat(fractionNum[1]);
@@ -16,6 +19,8 @@ function ConvertHandler() {
     else {
       result = parseFloat(result)
     }
+
+    //Default value to 1 for empty string
     if (result === null || result === "") { result = 1 }
     console.log(`getNum output ${result}`)
     return result;
@@ -25,9 +30,11 @@ function ConvertHandler() {
     let result;
     let regex = /[a-zA-Z%]+/g;
     result = input.match(regex)[0];
-    if (result === 'l') { result = "L" }
-    else if (result === 'L') { result = result }
+    //Set l to L and everything else in lowercase
+    if (result.toLowerCase() === 'l') { result = "L" }
     else { result = result.toLowerCase() }
+
+    //Check for invalid
     let check = this.getReturnUnit(result);
     if (check == "invalid unit") { result = check }
     return result;
@@ -82,7 +89,7 @@ function ConvertHandler() {
         result = "liters";
         break;
       default:
-        result = 'invalid unit'
+        result = 'invalid unit';
     }
     return result;
   };
@@ -93,29 +100,31 @@ function ConvertHandler() {
     const miToKm = 1.60934;
     let result = {};
     result.unit = this.getReturnUnit(initUnit);
-    switch (initUnit.toLowerCase()) {
-      case "gal":
-        result.num = roundNum(initNum * galToL);
-        break;
-      case "lbs":
-        result.num = roundNum(initNum * lbsToKg);
-        break;
-      case "mi":
-        result.num = roundNum(initNum * miToKm);
-        break;
-      case "l":
-        result.num = roundNum(initNum / galToL);
-        break;
-      case "kg":
-        result.num = roundNum(initNum / lbsToKg);
-        break;
-      case "km":
-        result.num = roundNum(initNum / miToKm);
-        break;
-    }
-    if (result.num === undefined) {
-      result.num = "invalid number"
-    }
+    if (result.unit.toLowerCase() !== "invalid unit" || initNum !== "invalid number") {
+      switch (initUnit.toLowerCase()) {
+        case "gal":
+          result.num = roundNum(initNum * galToL);
+          break;
+        case "lbs":
+          result.num = roundNum(initNum * lbsToKg);
+          break;
+        case "mi":
+          result.num = roundNum(initNum * miToKm);
+          break;
+        case "l":
+          result.num = roundNum(initNum / galToL);
+          break;
+        case "kg":
+          result.num = roundNum(initNum / lbsToKg);
+          break;
+        case "km":
+          result.num = roundNum(initNum / miToKm);
+          break;
+        default:
+          result.num = initNum
+      }
+    } else { result.num = initNum }
+
     console.log(`Input ${initNum} ${initUnit} and ${result.num} and ${result.unit}`)
     return result;
   };
@@ -129,9 +138,20 @@ function ConvertHandler() {
 
   this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
-    result = `${initNum} ${this.spellOutUnit(
-      initUnit
-    )} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
+    if (initNum === 'invalid number' && initUnit === 'invalid unit') {
+      result = 'invalid number and unit'
+    }
+    else if (initNum === 'invalid number') {
+      result = initNum
+    }
+    else if (initUnit === 'invalid unit') {
+      result = initNum
+    }
+    else {
+      result = `${initNum} ${this.spellOutUnit(
+        initUnit
+      )} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
+    }
     return result;
   };
 }
